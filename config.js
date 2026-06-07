f9a93332925f75d768aa08642c9affd923f9db22
Warren <esonwarr@gmail.com>
2026-06-07 18:50:23 +0000

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 
 // Your Google Apps Script Web App URL
 // (from Deploy → New deployment → Web app → copy the URL)
-const APPS_SCRIPT_URL = "YOUR_APPS_SCRIPT_URL_HERE";
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyjFAR0pQ-ErRN0sPsPrfRz3fisiMZz-Atb392qJ5xKxLEf5lUndZ1d571JhWbIHNaP/exec";
 
 // Your hostel name (shown in the header)
 const HOSTEL_NAME = "Hostel Register";

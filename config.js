e9095c7a67ea040b5a4b800e9642c3f64c37f4b6
Warren <esonwarr@gmail.com>
2026-06-07 17:42:08 +0000

    Update config.js

--- config.js
+++ config.js
@@ -5,11 +5,11 @@
 
 // 1. Your Supabase Project URL
 //    (Supabase dashboard → Settings → API → Project URL)
-const SUPABASE_URL = "YOUR_SUPABASE_URL_HERE";
+const SUPABASE_URL = "https://supabase.com/dashboard/project/tnxkiqvhcvieoqblsays";
 
 // 2. Your Supabase Anon/Public Key
 //    (Supabase dashboard → Settings → API → anon public)
-const SUPABASE_ANON_KEY = "YOUR_SUPABASE_ANON_KEY_HERE";
+const SUPABASE_ANON_KEY = "YeyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRueGtpcXZoY3ZpZW9xYmxzYXlzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA4NDM2NjgsImV4cCI6MjA5NjQxOTY2OH0.ELLWzfn6b7oeaWwCpDzdVmODDVFnLBA7y2OlHKfm0c0";
 
 // 3. Your hostel name (shown in the header)
 const HOSTEL_NAME = "Hostel Register";

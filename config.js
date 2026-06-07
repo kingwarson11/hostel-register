6d0ce79c658d481cca865dd363fd162548f63fe2
Warren <esonwarr@gmail.com>
2026-06-07 18:55:57 +0000

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 //  HOSTEL REGISTER — Configuration
 // ============================================================
 
-const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbyjFAR0pQ-ErRN0sPsPrfRz3fisiMZz-Atb392qJ5xKxLEf5lUndZ1d571JhWbIHNaP/exec";
+const APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzdtB--t7RUhz51EXaQNGQiDsik6oyFvDArvBUxFChsBY75CAEeuflKyJjaEF6Rm6k/exec";
 
 const HOSTEL_NAME = "Hostel Register";
 
